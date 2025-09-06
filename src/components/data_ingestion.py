541031d7003e8f25abaab68c8a7772a248bc2ddf
shahzaibkhan12345 <sk4563658@gmail.com>
2025-09-07 01:28:28 +0500

    Model has been Trained

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 
 @dataclass
 class DataIngestionConfig:
@@ -59,3 +62,7 @@ if __name__ == "__main__":
     train_arr, test_arr, _ = data_transformation.initiate_data_transformation(train_data, test_data)
     logging.info("Data Transformation done.")
     print("Data Transformation done")
+    logging.info("Starting model training process.")
+    model_trainer = ModelTrainer()
+    print("Best R2 Score:",model_trainer.initiate_model_trainer(train_arr, test_arr))
+    logging.info("Model training completed.")
